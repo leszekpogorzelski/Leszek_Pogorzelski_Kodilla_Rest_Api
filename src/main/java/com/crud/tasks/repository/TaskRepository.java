1643904497745897784e610caaac28d0b342dd25
Leszek <leszekpogorzelski1@gmail.com>
2019-05-15 17:11:01 +0200

    próba

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.crud.tasks.repository;
+
+import com.crud.tasks.domain.Task;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Id;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+/*@Transactional
+@Repository*/
+public interface TaskRepository extends CrudRepository<Task, Long> {
+    @Override
+    List<Task> findAll();
+
+    @Override
+    Optional<Task> findById(Long aLong);
+}
